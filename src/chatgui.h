9aa23d9905662e03dfd84b6a87fa1a491ae7cc1a
Roma Barge <l.roma.barge@tue.nl>
2020-07-27 11:57:37 +0200

    Updated chatgui header

--- src/chatgui.h
+++ src/chatgui.h
@@ -15,8 +15,9 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    
+    //ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
